4caf8e0cf7a46b17decb723522f7ced9dd197041
mrHovhannisyan <karenhovhannisyan01@gmail.com>
2022-08-18 17:47:35 +0400

    Add MUI dashboard component

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import SignIn from "./components/signin/Signin";
+// import SignIn from "./components/signin/Signin";
+import Dashboard from "./components/dashboard/Dashboard";
 function App() {
   return (
     <div className="App">
-      <SignIn/>
+      {/*<SignIn/>*/}
+      <Dashboard/>
     </div>
   );
 }
--- app/src/components/signin/Signin.js
+++ app/src/components/signin/Signin.js
@@ -5,13 +5,11 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 
 const theme = createTheme();
 
@@ -78,18 +76,6 @@ export default function SignIn() {
                         >
                             Sign In
                         </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
                     </Box>
                 </Box>
             </Container>
